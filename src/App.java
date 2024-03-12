3fb41d21c2f60b0a94e0ed21ccbae59a0228256f
Tiago Souza <tiagoeduardosouza@gmail.com>
2024-03-12 18:26:39 -0300

    Criacao da classe Carro

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
   public static void main(String[] args) {
 
+    Locadora locadora = new Locadora("New Car");
+
+    Carro monza = new Carro("Monza", 1990, "Chevrolet");
+    Carro fusca = new Carro("Fusca", 1970, "Volkswagen");
+    Carro kombi = new Carro("kombi", 1970, "Volkswagen");
+    Carro fiat = new Carro("Fiat Uno", 1990, "Fiat");
+
+    locadora.addCarro(monza);
+    locadora.addCarro(fusca);
+    locadora.addCarro(kombi);
+    locadora.addCarro(fiat);
+
+    System.out.println(locadora.getCarro());
+
   }
 }
